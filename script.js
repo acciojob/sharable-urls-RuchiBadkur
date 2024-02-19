96ad7c89458d0d5e140b8fe337b7498c8393694f
Ruchi Badkur <94377483+RuchiBadkur@users.noreply.github.com>
2024-02-19 13:32:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,20 +4,30 @@ const url = document.getElementById("url");
 const name = document.getElementById("name");
 const year = document.getElementById("year");
 const btn = document.getElementById("button");
-let urlFormate = url.textContent;
+let urlFormate = `https://localhost:8080/`;
 
 const updateUrl = (e) => {
-	// e.preventDefault();
-	if(name.value==="" && year.value===""){
-		urlFormate = url.textContent;
-	} else if(name.value !=="" && year.value !==""){
-		urlFormate += `?name=${name.value}&year=${year.value}`;
-	} else if(name.value !== "" && year.value===""){
-		urlFormate += `?name=${name.value};
-	} else if(name.value ==="" && year.value !==""){
-		urlFormate += `?year=${year.value}`;
-	} 
+	e.preventDefault();
+	
+	if(name.value && year.value){
+		urlFormate = `https://localhost:8080/?name=${name.value}&year=${year.value}`;
+	} else if(name.value){
+		urlFormate = `https://localhost:8080/?name=${name.value}`;
+
+	} else if(year.value ){
+		urlFormate = `https://localhost:8080/?year=${year.value}`;
+
+	}
+	else {
+		urlFormate = `https://localhost:8080/`;
+	}
+
+	url.textContent = urlFormate;
+	name.value = "";
+	year.value = "";
 }
 
+url.textContent = `https://localhost:8080/`;
 
 btn.addEventListener('click', updateUrl);
+
