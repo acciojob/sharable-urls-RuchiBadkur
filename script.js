e8c311cf375518defb8efbd3f8f9de9cd6cf7089
Ruchi Badkur <94377483+RuchiBadkur@users.noreply.github.com>
2024-02-19 12:57:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 // your code here
+// ?name=naman&year=2020
+const url = document.getElementById("url");
+const name = document.getElementById("name");
+const year = document.getElementById("year");
+const btn = document.getElementById("button");
+let urlFormate = url.textContent;
+
+const updateUrl = (e) => {
+	// e.preventDefault();
+	if(name.value==="" && year.value===""){
+		urlFormate = url.textContent;
+	} else if(name.value !=="" && year.value !==""){
+		urlFormate += `?name=${name.value}&year=${year.value}`;
+	} else if(name.value !== "" && year.value===""){
+		urlFormate += `?name=${name.value};
+	} else if(name.value ==="" && year.value !==""){
+		urlFormate += `?year=${year.value}`;
+	} 
+}
+
+
+btn.addEventListener('click', updateUrl);
